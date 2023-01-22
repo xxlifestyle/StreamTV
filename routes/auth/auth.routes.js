711da69d9ecd5052c3dbce7c0fafaa51163a4836
v.cherepanov <v.cherepanov@s-e.su>
2023-01-22 22:55:43 +0300

    delete compiled .js files

--- routes/auth/auth.routes.js
+++
@@ -1,9 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.authRouter = void 0;
-const express_1 = require("express");
-const users_controller_1 = require("../../controller/users.controller");
-const authRouter = (0, express_1.Router)();
-exports.authRouter = authRouter;
-authRouter.post("/login", users_controller_1.usersController.loginOne);
-authRouter.post("/register", users_controller_1.usersController.registerOne);
